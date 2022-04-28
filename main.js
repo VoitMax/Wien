7ad95165645fbf0eae927bfce650ea6c8345c9ef
VoitMax <maximilian.voit@student.uibk.ac.at>
2022-04-28 17:53:39 +0200

    Add featureGroup

--- main.js
+++ main.js
@@ -1,1 +1,45 @@
 /* OGD Wien Beispiel */
+
+let stephansdom = {
+    lat: 48.2085,
+    lng: 16.373,
+    title: "Stephansdom"
+};
+
+let startLayer = L.tileLayer.provider("BasemapAT.grau");
+
+let map = L.map("map", {
+    center: [stephansdom.lat, stephansdom.lng],
+    zoom: 12,
+    layers: [
+        startLayer
+    ]
+
+});
+
+let layerControl = L.control.layers({
+    "BasemapAT Grau": startLayer,
+    "BasemapAT Standard": L.tileLayer.provider("BasemapAT.basemap"),
+    "BasemapAT Orthophoto": L.tileLayer.provider("BasemapAT.orthofoto"),
+    "BasemapAT Gelände": L.tileLayer.provider("BasemapAT.terrain"),
+    "BasemapAT Oberfläche": L.tileLayer.provider("BasemapAT.surface"),
+    "BasemapAT High-DPI": L.tileLayer.provider("BasemapAT.highdpi"),
+    "BasemapAT Beschriftungen": L.tileLayer.provider("BasemapAT.overlay"),
+    "Basemap mit Orthofoto und Beschriftungen": L.layerGroup([
+        L.tileLayer.provider("BasemapAT.orthofoto"),
+        L.tileLayer.provider("BasemapAT.overlay"),
+    ])
+}).addTo(map);
+
+layerControl.expand(); 
+
+let sightLayer = L.featureGroup();
+
+layerControl.addOverlay(sightLayer, "Sehenswürdigkeiten");
+
+let mrk = L.marker([stephansdom.lat, stephansdom.lng]).addTo(sightLayer);
+
+
+
+
+
