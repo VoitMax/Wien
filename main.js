79c044a7eca7475357a3aeab37c6207984c33459
VoitMax <maximilian.voit@student.uibk.ac.at>
2022-05-12 16:04:55 +0200

    weight und dash arrray ergänzt

--- main.js
+++ main.js
@@ -197,7 +197,10 @@ async function loadLines(url) {
             };
 
             return {
-                color: `${colors[feature.properties.LINE_NAME]}`
+                color: `${colors[feature.properties.LINE_NAME]}`,
+                weight: 4,
+                dashArray: [10, 6]
+
             } 
         }
     }).bindPopup(function (layer) {
